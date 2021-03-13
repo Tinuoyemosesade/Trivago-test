df63b4cf23f91ba286c937a8a8aec64daf9c87bc
Moses Tinuoye <tinuoyemosesade@gmail.com>
2021-03-13 20:23:17 +0100

    Update book-management.spec.js

--- cypress/integration/book-management.spec.js
+++ cypress/integration/book-management.spec.js
@@ -1,9 +1,8 @@
 /// <reference types="cypress" />
 
-import { registerUser } from './test-utils'
+import { registerUser } from "./test-utils";
 
-describe('Book management', function() {
-
+describe("Book management", function () {
   beforeEach(() => {
     cy.visit("https://ts-e2e-challenge.netlify.app/list"); // step one, user should be able to visit the url
   });
@@ -12,14 +11,16 @@ describe('Book management', function() {
     registerUser();
 
     // Go to discover tab
-    cy.get('#root li:last-child').click()
+    cy.get("#root li:last-child").click();
 
     // Click the plus button
-    cy.get('li[aria-label = "How to Be an Antiracist"]').find('button[aria-label = "Add to list"]').first().click();
+    cy.get('li[aria-label = "How to Be an Antiracist"]')
+      .find('button[aria-label = "Add to list"]')
+      .first()
+      .click();
 
     // Go back to reading list and check that it is present
-    cy.get('nav').find('li:first-child').click();
-    cy.contains('How to Be an Antiracist');
+    cy.get("nav").find("li:first-child").click();
+    cy.contains("How to Be an Antiracist");
   });
-  
-})
+});
